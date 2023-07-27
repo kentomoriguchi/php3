f7b114c61d37a1e3c66b6c63c47f49799f23057a
kentomoriguchi <kento.moriguchi@mclogi.com>
2023-07-28 00:25:54 +0900

    修正

--- kadai_index.php
+++ kadai_index.php
@@ -12,19 +12,19 @@
 <header>
   <nav class="navbar navbar-default">
     <div class="container-fluid">
-    <div class="navbar-header"><a class="navbar-brand" href="select.php">データ一覧</a></div>
+    <div class="navbar-header"><a class="navbar-brand" href="kadai_select.php">データ一覧</a></div>
     </div>
   </nav>
 </header>
 <!-- Head[End] -->
 
 <!-- Main[Start] -->
-<form method="post" action="select.php">
+<form method="POST" action="kadai_insert.php">
   <div class="jumbotron">
    <fieldset>
     <legend>Gs Book</legend>
      <label>タイトル：<input type="text" name="title"></label><br>
-     <label>著者：<input type="text" name="author"></label><br>
+     <label>著者:<input type="text" name="author"></label><br>
      <label>URL:<input type="text" name="url"></label><br>
      <label>コメント:<textArea name="coment" rows="4" cols="40"></textArea></label><br>
      <input type="submit" value="送信">
